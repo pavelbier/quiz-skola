e3613858048c95559d8370626e60fb1c8d3aa2cf
Pavel Bier <pavel.bier@gmail.com>
2025-02-05 09:25:35 +0100

    Add files via upload

---
+++ src/components/vlastiveda_okolni_zeme.js
@@ -0,0 +1,157 @@
+export const vlastiveda_okolni_zeme_questions = [
+    {
+        question: 'Hlavní město Slovenska?',
+        options: ['Bratislava', 'Žilina', 'Varšava', 'Nitra'],
+        correctAnswer: 'Bratislava'
+    },
+    {
+        question: 'Kterým městem neprotéká Dunaj?',
+        options: ['Rakousko', 'Německo', 'Slovensko', 'Polsko'],
+        correctAnswer: 'Polsko'
+    },
+    {
+        question: 'Kolik obyvatel má Rakousko?',
+        options: ['8.2 milionu', '8 milionů', '8.4 milionů', '7.6 milionů'],
+        correctAnswer: '8.2 milionu'
+    },
+    {
+        question: 'V Rakousku je úřední jazyk?',
+        options: ['Němčina', 'Rakouština', 'Angličtina', 'Ruština'],
+        correctAnswer: 'Němčina'
+    },
+    {
+        question: 'Hlavní město Rakouska?',
+        options: ['Vídeň', 'Graz', 'Bratislava', 'Salzburg'],
+        correctAnswer: 'Vídeň'
+    },
+    {
+        question: 'Ve kterém země jsou velehory Alpy?',
+        options: ['Rakousko', 'Německo', 'Slovensko', 'Polsko'],
+        correctAnswer: 'Rakousko'
+    },
+    {
+        question: 'Největší řeka protékající Rakouskem?',
+        options: ['Dunaj', 'Morava', 'Nisa', 'Vltava'],
+        correctAnswer: 'Dunaj'
+    },
+    {
+        question: 'Největší jezero na hranici s Maďarskem se jmenuje?',
+        options: ['Neziderské', 'Modarinské', 'závody ponorek'],
+        correctAnswer: 'závody koní'
+    },
+    {
+        question: 'Počet obyvatel Slovenska je?',
+        options: ['5.4 milionu', '6 milionu', '6.4 milionu', '5 milionů'],
+        correctAnswer: '5.4 milionu'
+    },
+    {
+        question: 'Nejdelší řeka Slovenska je?',
+        options: ['Váh', 'Vltava', 'Nisa', 'Morava'],
+        correctAnswer: 'Váh'
+    },
+    {
+        question: 'Ve které zemi se neplatí eurem?',
+        options: ['Slovensko', 'Polsko', 'Německo', 'Rakousko'],
+        correctAnswer: 'Polsko'
+    },
+    {
+        question: 'Druhé nejlidnatější město na východě Slovenska je?',
+        options: ['Košice', 'Trenčín', 'Žilina', 'Banské Bystrica'],
+        correctAnswer: 'Košice'
+    },
+    {
+        question: 'Jaký průmysl je na Slovensku nejdůležitější?',
+        options: ['Automobilový', 'Chemický', 'Hutnický', 'Strojírenský'],
+        correctAnswer: 'Automobilový'
+    },
+    {
+        question: 'Jak se jmenoval legendární zbojník ze Slovenska?',
+        options: ['Jánošík', 'Lotrando', 'Grasel', 'Sobri'],
+        correctAnswer: 'Jánošík'
+    },
+    {
+        question: 'Hlavní město Polska je?',
+        options: ['Varšava', 'Katovice', 'Vratislav', 'Poznaň'],
+        correctAnswer: 'Varšava'
+    },
+    {
+        question: 'Většinu polská pokrývá nížina co se jmenuje?',
+        options: ['Velkopolská', 'Severmořská', 'Baltská', 'Podněmecká'],
+        correctAnswer: 'Velkopolská'
+    },
+    {
+        question: 'Nejdelší polské řeky jsou?',
+        options: ['Odra a Visla', 'Nisa a Niva','Vltava a Labe', 'Váh a Hron'],
+        correctAnswer: 'Odra a Visla'
+    },
+    {
+        question: 'Počet obyvatel Polska je?',
+        options: ['38.6 milionů', '48.6 milionů', '28.6 milionů'],
+        correctAnswer: '38.6 milionů'
+    },
+    {
+        question: 'Největší polský námořní přístav je?',
+        options: ['Gdaňsk', 'Hamburk', 'Kaliningrad', 'Rostock'],
+        correctAnswer: 'Gdaňsk'
+    },
+    {
+        question: 'Moře na severu Polska je?',
+        options: ['Baltské', 'Severní', 'Středozemní'],
+        correctAnswer: 'Baltské'
+    },    
+    {
+        question: 'Největší polský námořní přístav je?',
+        options: ['Gdaňsk', 'Hamburk', 'Kaliningrad', 'Rostock'],
+        correctAnswer: 'Gdaňsk'
+    },
+    {
+        question: 'Hlavní město Německa je?',
+        options: ['Berlín', 'Hamburk', 'Frankfurt', 'Kolín nad Rýnem'],
+        correctAnswer: 'Berlín'
+    },
+    {
+        question: 'Počet obyvatel Německa je?',
+        options: ['83 milionů', '93 milionů', '88 milionů', '73 milionů'],
+        correctAnswer: '83 milionů'
+    },
+    {
+        question: 'Největší soused ČR je?',
+        options: ['Německo', 'Rakousko', 'Slovensko', 'Maďarsko'],
+        correctAnswer: 'Německo'
+    },
+    {
+        question: 'Nejmenší soused ČR je?',
+        options: ['Německo', 'Rakousko', 'Slovensko', 'Maďarsko'],
+        correctAnswer: 'Slovensko'
+    },
+    {
+        question: 'Počet obyvatel Německa je?',
+        options: ['83 milionů', '93 milionů', '88 milionů', '73 milionů'],
+        correctAnswer: '83 milionů'
+    },
+    {
+        question: 'Moře na severu Německa se jmenují?',
+        options: ['Severní a Baltské', 'Severní a Středozemní', 'Baltské a Středozemní', 'Severní a Tichý'],
+        correctAnswer: 'Severní a Baltské'
+    },
+    {
+        question: 'Největší německý ostrov v Baltském moři se jmenuje?',
+        options: ['Rujana', 'Rihana', 'Rajana', 'Rehjana'],
+        correctAnswer: 'Rujana'
+    },
+    {
+        question: 'Podél které řeky v Německu leží nejvíce průmyslových závodů?',
+        options: ['Rýn', 'Labe', 'Vltava', 'Mohan'],
+        correctAnswer: 'Rýn'
+    },
+    {
+        question: 'Největší síť dálnic má?',
+        options: ['Německo', 'Rakousko', 'Slovensko', 'Polsko'],
+        correctAnswer: 'Německo'
+    },
+    {
+        question: 'Největší německé letiště je ve městě?',
+        options: ['Frankfurt nad Mohanem', 'Mnichov', 'Kolín nad Rýnem', 'Hamburk'],
+        correctAnswer: 'Frankfurt nad Mohanem'
+    }
+];
