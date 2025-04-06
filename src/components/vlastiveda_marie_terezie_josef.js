818308c7256401e5c3f1ae916db754c44e67429f
Pavel Bier <pavel.bier@seznam.cz>
2025-04-06 21:24:32 +0200

    fixes

---
+++ src/components/vlastiveda_marie_terezie_josef.js
@@ -0,0 +1,108 @@
+export const vlastiveda_marie_terezie_josef_questions = [
+    {
+        question: 'Lidé se měli za vlády Marie Terezie a Josefa II?',
+        options: ['Více řidit svým rozumem a vědeckými poznatky', 'Poslouchat víc své kněze', 'Stavět sněhuláky'],
+        correctAnswer: 'Více řidit svým rozumem a vědeckými poznatky'
+    },
+    {
+        question: 'Marie Terezie a Josef II patřili k rodu?',
+        options: ['Habsburkové', 'Jagelonci', 'Přemyslovci', 'Lucemburkové'],
+        correctAnswer: 'Habsburkové'
+    },
+    {
+        question: 'Marie Terezie nařídila, že robotovat se může maximálně kolik dní?',
+        options: ['Dva', 'Tři', 'Čtyři', 'Pět'],
+        correctAnswer: 'Tři'
+    },
+    {
+        question: 'Marie Terezie zavedla roku 1774 povinnou školní docházku pro děti ve věku?',
+        options: ['6-12', '5-12', '6-13', '5-13'],
+        correctAnswer: '6-12'
+    },
+    {
+        question: 'Děti se měly ve škole hlavně naučit tři věci?',
+        options: ['číst, psát a počítat', 'číst, psát a malovat', 'číst, počítat a mluvit', 'mluvit, myslet, zpívat'],
+        correctAnswer: 'číst, psát a počítat'
+    },        
+    {
+        question: 'Marie Terezie rozdělila zemi na?',
+        options: ['kraje', 'okresy', 'vévodství'],
+        correctAnswer: 'kraje'
+    },        
+    {
+        question: 'Josef II roku 1781 zrušil?',
+        options: ['nevolnictví', 'zemědělství', 'náboženství'],
+        correctAnswer: 'nevolnictví'
+    },        
+    {
+        question: 'Kolik patentů (zákonů) Josef II zavedl?',
+        options: ['5000', '6000', '7000', '8000'],
+        correctAnswer: '6000'
+    },        
+    {
+        question: 'Nevolnictví znamenalo, že nesmí bez svolení pána?',
+        options: ['modlit, zpívat, hrát', 'stěhovat, ženit a učit se čemu chtějí', 'věřit v boha'],
+        correctAnswer: 'stěhovat, ženit a učit se čemu chtějí'
+    },        
+    {
+        question: 'Josef II povolil další náboženství než dosavadní, které bylo?',
+        options: ['katolické', 'protestantské', 'budhistické'],
+        correctAnswer: 'katolické'
+    },        
+    {
+        question: 'Josef II ze zrušených klášterů udělal?',
+        options: ['nemocnice, sirotčince, kasárna pro vojáky', 'kovárny a pily', 'lesní školku'],
+        correctAnswer: 'nemocnice, sirotčince, kasárna pro vojáky'
+    },        
+    {
+        question: 'Josef II řemesla, obchod a podnikání?',
+        options: ['podporoval', 'nepodporoval', 'bylo mu to jedno'],
+        correctAnswer: 'podporoval'
+    },        
+    {
+        question: 'Za vlády Marie Terezie a Josefa II se pěstovalo hlavně?',
+        options: ['obilí', 'řepa', 'slunečnice'],
+        correctAnswer: 'obilí'
+    },        
+    {
+        question: 'Za vlády Marie Terezie a se začaly pěstovat?',
+        options: ['brambory', 'káva', 'vinná réva'],
+        correctAnswer: 'brambory'
+    },
+    {
+        question: 'Za vlády Marie Terezie venkovani jedli hlavně?',
+        options: ['luštěniny, kaši, málo masa', 'věci z fast foodu', 'kořínky v lese'],
+        correctAnswer: 'luštěniny, kaši, málo masa'
+    },
+    {
+        question: 'Brambory se dostaly do Evropy z?',
+        options: ['Ameriky', 'Anglie', 'Austrálie'],
+        correctAnswer: 'Ameriky'
+    },
+    {
+        question: 'Kolik dětí měla Marie Terezie?',
+        options: ['14','15', '16', '17'],
+        correctAnswer: '16'
+    },
+    {
+        question: 'Příjmení zavedl(a)?',
+        options: ['Marie Terezie','Josef II'],
+        correctAnswer: 'Marie Terezie'
+    },
+    {
+        question: 'Povinnou školní docházku zavedl(a)?',
+        options: ['Marie Terezie','Josef II'],
+        correctAnswer: 'Marie Terezie'
+    },    
+    {
+        question: 'Čísla na domech zavedl(a)?',
+        options: ['Marie Terezie','Josef II'],
+        correctAnswer: 'Marie Terezie'
+    },
+    {
+        question: 'Více náboženství povolil(a)?',
+        options: ['Marie Terezie','Josef II'],
+        correctAnswer: 'Josef II'
+    },
+
+];
